6e4650a6fab723e0a27d2fb80d5f1a79bc9419f8
Rizka Aulia <rizkauliaa2715@gmail.com>
2025-11-18 23:08:33 +0700

    Delete Kode Program directory

--- Kode Program/views/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../controllers/LecturerController.php";
-include_once("../controllers/SubjectController.php");
-
-$controller = new LecturerController();
-$controller->index();
-
-$subject = new SubjectController();
-$subject->index();
